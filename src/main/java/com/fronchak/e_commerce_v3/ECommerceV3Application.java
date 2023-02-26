ecbf257284d8fed189f1f27b03698c6cca52b283
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2023-02-26 20:59:40 -0300

    Create project

---
+++ src/main/java/com/fronchak/e_commerce_v3/ECommerceV3Application.java
@@ -0,0 +1,13 @@
+package com.fronchak.e_commerce_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fronchak/e_commerce_v3/ECommerceV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fronchak.e_commerce_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
